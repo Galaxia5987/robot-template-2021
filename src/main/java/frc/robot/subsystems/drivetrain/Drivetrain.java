190da51c6c32b2441b9889d0aff364507c3442d9
Orel David Aboukasis <orel.david1502@gmail.com>
2020-08-07 13:10:13 +0300

    Create the motors for the drivetrain

--- src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
@@ -1,13 +1,21 @@
 package frc.robot.subsystems.drivetrain;
 
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Ports;
 
 /**
  * An example subsystem.  You can replace me with your own Subsystem.
  */
 public class Drivetrain extends SubsystemBase {
-// Put methods for controlling this subsystem
-// here. Call these from Commands.
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+    TalonFX motorRightFront = new TalonFX(Ports.Drivetrain.MOTOR_RIGHT_FRONT);
+    TalonFX motorRightBack = new TalonFX(Ports.Drivetrain.MOTOR_RIGHT_BACK);
+    TalonFX motorLeftFront = new TalonFX(Ports.Drivetrain.MOTOR_LEFT_FRONT);
+    TalonFX motorLeftBack = new TalonFX(Ports.Drivetrain.MOTOR_LEFT_BACK);
+
+
 
     public Drivetrain(){
 

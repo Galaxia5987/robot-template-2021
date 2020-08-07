6ee58e5eaeb26faf52d8d7f60eb9a79db38d6da5
Orel David Aboukasis <orel.david1502@gmail.com>
2020-08-07 13:17:55 +0300

    Config directions for the motors and follow

--- src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
@@ -12,14 +12,22 @@ import frc.robot.subsystems.UnitModel;
 public class Drivetrain extends SubsystemBase {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
-    TalonFX motorRightFront = new TalonFX(Ports.Drivetrain.MOTOR_RIGHT_FRONT);
-    TalonFX motorRightBack = new TalonFX(Ports.Drivetrain.MOTOR_RIGHT_BACK);
     TalonFX motorLeftFront = new TalonFX(Ports.Drivetrain.MOTOR_LEFT_FRONT);
     TalonFX motorLeftBack = new TalonFX(Ports.Drivetrain.MOTOR_LEFT_BACK);
+    TalonFX motorRightFront = new TalonFX(Ports.Drivetrain.MOTOR_RIGHT_FRONT);
+    TalonFX motorRightBack = new TalonFX(Ports.Drivetrain.MOTOR_RIGHT_BACK);
     UnitModel unitModel = new UnitModel(Constants.Drivetrain.TICKS_TO_METER);
 
 
     public Drivetrain(){
+        motorLeftBack.follow(motorLeftFront);
+        motorRightBack.follow(motorRightFront);
+
+        motorLeftFront.setInverted(Ports.Drivetrain.LEFT_INVERTED);
+        motorLeftBack.setInverted(Ports.Drivetrain.LEFT_INVERTED);
+
+        motorRightFront.setInverted(Ports.Drivetrain.RIGHT_INVERTED);
+        motorRightBack.setInverted(Ports.Drivetrain.RIGHT_INVERTED);
 
     }
 

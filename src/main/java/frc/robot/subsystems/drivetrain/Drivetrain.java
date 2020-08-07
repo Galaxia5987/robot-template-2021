8419f5a71701975b0faf25728df3650f5edbab85
Orel David Aboukasis <orel.david1502@gmail.com>
2020-08-07 13:04:28 +0300

    Create subsystem file
    For the drivetrain

---
+++ src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems.drivetrain;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/**
+ * An example subsystem.  You can replace me with your own Subsystem.
+ */
+public class Drivetrain extends SubsystemBase {
+// Put methods for controlling this subsystem
+// here. Call these from Commands.
+
+    public Drivetrain(){
+
+    }
+
+    @Override
+    public void periodic() {
+
+    }
+}

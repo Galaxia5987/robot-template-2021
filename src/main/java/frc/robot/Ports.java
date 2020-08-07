8dc02c25d7ff5496999191c95f2c6d9f538d386f
Orel David Aboukasis <orel.david1502@gmail.com>
2020-08-07 13:17:18 +0300

    Add inversion constants for the motors

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -7,5 +7,8 @@ public class Ports {
         public static final int MOTOR_RIGHT_BACK = 1;
         public static final int MOTOR_LEFT_FRONT = 2;
         public static final int MOTOR_LEFT_BACK = 3;
+
+        public static final boolean RIGHT_INVERTED = false;
+        public static final boolean LEFT_INVERTED = true;
     }
 }

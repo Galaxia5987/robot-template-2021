282d2e58d61b007e9deef12657997cdaae586cbc
Orel David Aboukasis <orel.david1502@gmail.com>
2020-08-07 13:09:54 +0300

    Create ports file
    with the ports of the drivetrain

---
+++ src/main/java/frc/robot/Ports.java
@@ -0,0 +1,11 @@
+package frc.robot;
+
+public class Ports {
+
+    public static class Drivetrain{
+        public static final int MOTOR_RIGHT_FRONT = 0;
+        public static final int MOTOR_RIGHT_BACK = 1;
+        public static final int MOTOR_LEFT_FRONT = 2;
+        public static final int MOTOR_LEFT_BACK = 3;
+    }
+}

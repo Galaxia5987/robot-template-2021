38c5d1bce1eacef499ada869049b98499f6970d4
oreld <orel.david1502@gmail.com>
2020-08-09 12:47:23 +0300

    Fix ports

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -3,10 +3,10 @@ package frc.robot;
 public class Ports {
 
     public static class Drivetrain{
-        public static final int MOTOR_RIGHT_FRONT = 0;
-        public static final int MOTOR_RIGHT_BACK = 1;
-        public static final int MOTOR_LEFT_FRONT = 2;
-        public static final int MOTOR_LEFT_BACK = 3;
+        public static final int MOTOR_RIGHT_FRONT = 12;
+        public static final int MOTOR_RIGHT_BACK = 13;
+        public static final int MOTOR_LEFT_FRONT =10;
+        public static final int MOTOR_LEFT_BACK = 11;
 
         public static final boolean RIGHT_INVERTED = false;
         public static final boolean LEFT_INVERTED = true;
